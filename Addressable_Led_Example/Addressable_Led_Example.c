f564595bd1bb0ca163355b84a290c5b049848945
jfritz022 <jfritz022@gmail.com>
2015-06-24 23:47:38 -0400

    License and BRIGHTNESS Change
    Added in a license and basic usage instructions.

--- Addressable_Led_Example/Addressable_Led_Example.c
+++ Addressable_Led_Example/Addressable_Led_Example.c
@@ -1,8 +1,32 @@
 /*
- * Addressable_Led_Example.c
- *
- * Created: 6/24/2015 5:36:15 PM
- *  Author: John
+Addressable_Led_Example.c
+
+Example program for new AdaFruit serial RGB LED strips.
+
+To use:
+	- In spi.h, change #define macros to match your ports and pin numbers
+	- In Addressable_Led_Example.c, define your F_CPU and desired global brightness anywhere from 0b00000 to 0b11111 (this is a multiplier of the brightness you set for each LED).
+	- In Addressable_Led_Example.c, define the total number of LEDS in your strip. 
+
+Copyright (c) 2015 John S Fritz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
  */ 
 
 #define F_CPU 16000000L
@@ -25,6 +49,7 @@ int main(void) {
 	
 	SPIinit();
 	UARTinit();
+	BRIGHTNESS |= 0b11100000;		//makes sure first frame is setup correctly
 	
     while(1) {
 		
