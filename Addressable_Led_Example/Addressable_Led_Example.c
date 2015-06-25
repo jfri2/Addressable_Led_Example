2affe3772f0941d997f7aa1f9da73b60d963e79a
jfritz022 <jfritz022@gmail.com>
2015-06-24 22:51:13 -0400

    Untested prototype of led sequence

--- Addressable_Led_Example/Addressable_Led_Example.c
+++ Addressable_Led_Example/Addressable_Led_Example.c
@@ -12,12 +12,63 @@
 #include "serial.c"
 #include "spi.h"
 
+uint8_t BRIGHTNESS = 0xFF;
+
+void sendStartFrame(void);
+void sendStopFrame(void);
+void sendLedColorFrame(uint8_t, uint8_t, uint8_t);
+void sendLedSelectFrame(uint32_t);
+void sendAllFrames(uint32_t, uint8_t, uint8_t, uint8_t);
+
 int main(void) {
 	
 	SPIinit();
 	UARTinit();
 	
     while(1) {
-        //TODO:: Please write your application code 
+        for(int i = 0; i < 144; i++) {
+			sendAllFrames(i,100,100,100);
+		}
     }
+}
+
+void sendStartFrame(void) {	
+	for(uint8_t i = 0; i < 4; i++) {
+		SPIxchng(0);
+	}
+}
+
+void sendStopFrame(void) {
+	for(uint8_t i = 0; i < 4; i++) {
+		SPIxchng(0xFF);
+	}
+}
+
+void sendLedColorFrame(uint8_t red, uint8_t green, uint8_t blue) {
+	SPIxchng(BRIGHTNESS);
+	SPIxchng(blue);
+	SPIxchng(green);
+	SPIxchng(red);
+}
+
+void sendLedSelectFrame(uint32_t ledNumber) {
+	uint8_t selectFrame[4];
+	
+	selectFrame[0] = (ledNumber >> 24) & 0xFF;
+	selectFrame[1] = (ledNumber >> 16) & 0xFF;
+	selectFrame[2] = (ledNumber >> 8) & 0xFF;
+	selectFrame[3] = ledNumber & 0xFF;
+	
+	for(uint8_t i = 0; i < 4; i++) {
+		SPIxchng(selectFrame[i]);
+	}
+}
+
+void sendAllFrames(uint32_t ledNumber, uint8_t red, uint8_t green, uint8_t blue) {
+	SPI_START();	
+	sendStartFrame();
+	sendLedColorFrame(red, green, blue);
+	sendLedSelectFrame(ledNumber);
+	sendStopFrame();	
+	SPI_END();
 }
